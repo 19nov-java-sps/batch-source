d21d4aa13dab97f005ee2bd1ced71dbae18a77cd
sekoudosso82 <sekou.dosso82@gmail.com>
2019-11-06 09:23:39 -0500

    initial commit

---
+++ shapes/src/shapes/Shape.java
@@ -0,0 +1,24 @@
+package shapes;
+
+
+public abstract class Shape 
+{
+	
+	private int numOfSides;
+	
+	public Shape() 
+	{
+		super();
+	}
+	
+	public int getNumOfSides() 
+	{
+		return numOfSides;
+	}
+	
+	public void setNumOfSides(int numOfSides) 
+	{
+		this.numOfSides = numOfSides;
+	}
+
+}
