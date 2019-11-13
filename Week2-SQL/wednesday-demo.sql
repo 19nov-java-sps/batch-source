982e6f05caa8bd97f53c2591dcf7aa57e9a4e2ea
carolynrehm <carolynhrehm@gmail.com>
2019-11-13 16:24:35 -0500

    set operations + subqueries + joins + functions

--- Week2-SQL/wednesday-demo.sql
+++ Week2-SQL/wednesday-demo.sql
@@ -69,3 +69,160 @@ order by monthly_salary desc;
 select dept_id as id, dept_name as department 
 from department;
 
+select empl_name, empl_position
+from employee 
+where empl_name like 'Huey%';
+
+
+----------------------------------
+-- Using functions in our queries
+----------------------------------
+select count(empl_id)
+from employee;
+
+select count(empl_id) as "Employee Count", dept_id as "Respective Dept Id"
+from employee
+group by dept_id;
+
+select count(empl_id) as "Employee Count", dept_id as "Respective Dept Id"
+from employee
+group by dept_id
+having dept_id = 1;
+
+select count(empl_id) as "Employee Count", dept_id as "Respective Dept Id"
+from employee
+where monthly_salary>3900
+group by dept_id;
+
+-- both using avg aggreate function nad the round scalar function
+select round(avg(monthly_salary),2) as avg_sal, dept_id as dept
+from employee
+group by dept_id;
+
+------------------------
+-- sub queries 
+------------------------
+select *
+from employee 
+where dept_id =
+	(select dept_id
+	from department
+	where dept_name = 'HR')
+	
+
+select *
+from employee
+where monthly_salary = 
+	(select max(monthly_salary)
+	from employee);
+
+select *
+from employee
+where monthly_salary = 
+	(select min(monthly_salary)
+	from employee);
+
+------------------------
+-- set operations (union [all], intersect, except)
+------------------------
+
+select * 
+from employee
+where monthly_salary>3500
+union
+select *
+from employee
+where dept_id=1;
+
+select *
+from employee
+where monthly_salary>3500 or dept_id=1;
+
+select * 
+from employee
+where monthly_salary>3500
+union all
+select *
+from employee
+where dept_id=1;
+
+
+select * 
+from employee
+where monthly_salary>3500
+intersect
+select *
+from employee
+where dept_id=1;
+
+
+select * 
+from employee
+where monthly_salary>3500
+except
+select *
+from employee
+where dept_id=1;
+
+
+------------------------
+-- JOINS
+------------------------
+
+insert into department values (default, 'Accounting', 8000);
+insert into employee values (15,'Greg Ronaldo', 5000, 'CEO', null, null);
+
+-- inner join with implicit column alias
+select employee.empl_name "Name", department.dept_name "Department"
+from employee
+join department
+on employee.dept_id = department.dept_id;
+
+select e.empl_name "Name", d.dept_name "Department"
+from employee e
+join department d
+on d.dept_id = e.dept_id;
+
+select e.empl_name "Name", d.dept_name "Department"
+from employee e
+inner join department d
+on d.dept_id = e.dept_id
+where d.dept_name='Marketing';
+
+select e.empl_name "Name", d.dept_name "Department"
+from employee e
+full outer join department d
+on d.dept_id = e.dept_id;
+
+select e.empl_name "Name", d.dept_name "Department"
+from employee e
+left outer join department d
+on d.dept_id = e.dept_id;
+
+select e.empl_name "Name", d.dept_name "Department"
+from employee e
+right outer join department d
+on d.dept_id = e.dept_id;
+
+select e.empl_name "Name", d.dept_name "Department"
+from employee e
+cross join department d;
+
+-- inner join vs natural join 
+
+select e.*, d.*
+from employee e
+natural join department d;
+
+
+select employee.*, department.*
+from employee
+join department
+on employee.dept_id = department.dept_id;
+
+
+-- self join 
+select e.empl_name "Employee", e.empl_position "E Role", m.empl_name "Manager", m.empl_position "M Role"
+from employee e
+join employee m 
+on e.manager_id = m.empl_id;
