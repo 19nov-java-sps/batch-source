51565bca38302cacc28479352a745484009d8c7c
kwamemartinj <kwame.martin.j@gmail.com>
2019-11-21 04:32:01 -0500

    hmm

--- OOPExercise/src/com/revature/animals/AnimalInterface.java
+++
@@ -1,6 +0,0 @@
-package com.revature.animals;
-
-public interface AnimalInterface {
-	public void eat();
-	public void sound();
-}
--- OOPExercise/src/com/revature/animals/BirdInterface.java
+++
@@ -1,8 +0,0 @@
-package com.revature.animals;
-
-public interface BirdInterface {
-	int numberOfLegs = 2;
-	String outerCovering = "feathers";
-	
-	public void fly();
-}
