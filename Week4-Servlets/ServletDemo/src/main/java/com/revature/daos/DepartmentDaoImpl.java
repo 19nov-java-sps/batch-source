7b44e3cb91c50acae0c39a0b4a26d036bfd8a5ba
carolynrehm <carolynhrehm@gmail.com>
2019-11-26 15:59:43 -0500

    servlet demo

--- Week4-Servlets/ServletDemo/src/main/java/com/revature/daos/DepartmentDaoImpl.java
+++ Week4-Servlets/ServletDemo/src/main/java/com/revature/daos/DepartmentDaoImpl.java
@@ -12,8 +12,8 @@ public class DepartmentDaoImpl implements DepartmentDao {
 	public DepartmentDaoImpl() {
 		departments.add(new Department(1,"Marketing",13000));
 		departments.add(new Department(2,"Human Resources",10000));
-//		departments.add(new Department(3,"Information Technology",8000));
-		departments.add(new Department(4,"Legal",9000));
+		departments.add(new Department(3,"Information Technology",8000));
+//		departments.add(new Department(4,"Legal",9000));
 	}
 	
 	@Override
---
+++ Week4-Servlets/ServletDemo/src/main/java/com/revature/servlets/api/EmployeeServlet.java
@@ -0,0 +1,68 @@
+package com.revature.servlets.api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.models.Department;
+import com.revature.models.Employee;
+import com.revature.services.EmployeeService;
+
+/**
+ * Servlet implementation class EmployeeServlet
+ */
+public class EmployeeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	private EmployeeService employeeService = new EmployeeService();
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EmployeeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		List<Employee> employees = employeeService.getAll();	
+		
+		ObjectMapper om = new ObjectMapper();
+		String employeeJSON = om.writeValueAsString(employees);
+		
+		try(PrintWriter pw = response.getWriter()){
+			pw.write(employeeJSON);
+		}
+		
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String newName = request.getParameter("name");
+		String newDeptId = request.getParameter("dept");
+//		System.out.println(newName + " in dept "+newDeptId);
+		
+		Employee e = new Employee();
+		e.setName(newName);
+	
+		Department d = new Department();
+		//pls do more input validation than this vvvv
+		d.setId(Integer.parseInt(newDeptId));
+		e.setDepartment(d);
+		
+		employeeService.create(e);
+		
+//		response.setStatus(201);
+		response.sendRedirect("directory");
+		
+	}
+
+}
