b3b4757b0edf0172e04ef3f9b13cd4671916e2ee
Ryan_Carstons <carsryan@gmail.com>
2019-11-27 14:10:37 -0500

    update

--- Week4-Servlets/ServletDemo/src/main/java/com/revature/daos/DepartmentDaoImpl.java
+++ Week4-Servlets/ServletDemo/src/main/java/com/revature/daos/DepartmentDaoImpl.java
@@ -14,10 +14,9 @@ public class DepartmentDaoImpl implements DepartmentDao {
 		departments.add(new Department(2,"Human Resources",10000));
 		departments.add(new Department(3,"Information Technology",8000));
 //		departments.add(new Department(4,"Legal",9000));
-<<<<<<< HEAD
-=======
+
 		departments.add(new Department(5,"Catering",15000));
->>>>>>> Carolyn_Rehm
+
 	}
 	
 	@Override
