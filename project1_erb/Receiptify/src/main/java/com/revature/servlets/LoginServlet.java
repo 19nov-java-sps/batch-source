cd1a56920d30789c2a807a12d622fd2920771474
Peter Nguyen <peter.nguyen17@gmail.com>
2019-11-27 00:25:16 -0500

    mkdir structure  for project 1

---
+++ project1_erb/Receiptify/src/main/java/com/revature/servlets/LoginServlet.java
@@ -0,0 +1,28 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("get hit");
+		request.getRequestDispatcher("Views/Login.html").forward(request, response);
+		
+	}
+
+
+}
