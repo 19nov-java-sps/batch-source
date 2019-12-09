08b88729ecd17444fffc95f5e63649bd48223c25
Peter Nguyen <peter.nguyen17@gmail.com>
2019-12-09 05:38:50 -0500

    tour of heros

---
+++ Tour of Heros/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { HEROES }  from "../mock-heroes"
+import { Hero } from '../hero';
+import { HeroService } from "../hero.service"
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes = Hero[];
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes()
+  }
+
+  getHeroes(): void {
+      this.heroService.getHeroes().subscribe(heroes => (
+        this.heroes =heroes
+      ))
+  }
+  
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+    console.log(this.selectedHero)
+  }
+
+
+}
