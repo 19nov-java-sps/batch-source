ddcdae71a441151357fca3921e1bb441bae4a083
Peter Nguyen <peter.nguyen17@gmail.com>
2019-12-10 05:32:04 -0500

    refactoring css and starting hibernate

--- expensify/src/main/webapp/static/Scripts/landing.js
+++ expensify/src/main/webapp/static/Scripts/landing.js
@@ -1,20 +1,33 @@
+//Variables
 let token = sessionStorage.getItem("token");
-const currentUser = token.split(":")[0]
 let invoiceURL = "http://localhost:8080/expensify/api/invoices"
 let employeesURL = "http://localhost:8080/expensify/employees"
-	
-document.getElementById("fetchInvoice").addEventListener("click", sendAjaxGetInvoices)
-document.getElementById("fetchResolved").addEventListener("click", fetchResolveInvoices)
+const currentUser = token.split(":")[0]
+
+//EventListeners
+
+document.getElementById("fetchInvoice").addEventListener("click", fetchInvoices)
 document.getElementById("fetchEmployee").addEventListener("click", fetchEmployees)
+document.getElementById("fetchResolved").addEventListener("click", fetchResolveInvoices)
+
 document.getElementById("editEmployee").addEventListener("click", forwardToEditPage)
-document.getElementById("findInvoices").addEventListener("click", invoiceSearchByEmployeeId)
-document.getElementById("signOut").addEventListener("click", logOut)
+
 document.getElementById("createInvoices").addEventListener("click", create)
 document.getElementById("submit-btn").addEventListener("click", createNewInvoice)
+
 document.getElementById("pendingInvoices").addEventListener("click", fetchMyPendingInvoices)
 document.getElementById("myResolvedInvoice").addEventListener("click", fetchMyResolvedInvoices)
+
+
+document.getElementById("findInvoices").addEventListener("click", invoiceSearchByEmployeeId)
+document.getElementById("signOut").addEventListener("click", logOut)
+
+
 document.getElementById("approve-btn").addEventListener("click", approveInvoice)
 document.getElementById("deny-btn").addEventListener("click", denyInvoice)
+
+
+
 
 
 if(!token){
@@ -28,6 +41,123 @@ if(!token){
 		window.location.href="http://localhost:8080/expensify/hello";
 	}
 }
+
+
+
+function sendAjaxGet(url, callback){
+	let xhr = new XMLHttpRequest();
+	xhr.open("GET", url);
+	xhr.onreadystatechange = function(){
+		if(this.readyState === 4 && this.status === 200) {
+			callback(this);
+		} 
+	}
+	xhr.setRequestHeader("Authorization", token);
+	xhr.send();
+}
+
+function displayInfo(xhr) {
+	let empl = JSON.parse(xhr.response);
+	console.log(empl)
+	document.getElementById("full_name").innerHTML = empl.fullname
+	let image = document.getElementById('pic')
+	image.src = `https://mdbootstrap.com/img/Photos/Avatars/img%20(${currentUser}).jpg`
+	if (empl.manager === true) {
+		document.getElementById("search").hidden = false
+		document.getElementById("manager_status").innerHTML = "Manager"
+		document.getElementById("fetchInvoice").hidden = false
+		document.getElementById("fetchEmployee").hidden = false
+		document.getElementById("approveOrDeny").hidden = false
+		console.log("A manager logged in")
+	} else {
+		document.getElementById("fetchInvoice").hidden = true
+		document.getElementById("approveOrDeny").style.display = "none"
+		document.getElementById("fetchEmployee").hidden = true
+		document.getElementById("search").hidden = true
+		document.getElementById("fetchResolved").hidden = true
+		console.log("A regular employee logged in")
+	}
+}
+
+
+// ADMIN Fetch All Pending Invoices, Fetch Employees,  Fetch All Resolved Invoices
+
+function fetchInvoices(){
+	fetch("http://localhost:8080/expensify/api/invoices", {
+		headers: {
+			Authorization: `${token}`
+		}
+	})
+	.then(res => {
+		if (res.status === 200) {
+			return res.json()
+		} else {
+			console.log("error")
+		}
+		})
+			.then(invoices => {
+				let pendingListContainer = document.getElementById("pendingListContainer")
+				for (let invoice of invoices){
+					if (invoice.pending === true) {
+						console.log(invoice)
+						
+						let invoiceList = document.createElement("div")
+						invoiceList.setAttribute("class", "media")
+						let image = document.createElement("img")
+						image.setAttribute("class", "mr-3")
+						image.src= `https://mdbootstrap.com/img/Photos/Avatars/img%20(${invoice.userId}).jpg`
+						let mediaBody  = document.createElement("div")
+						mediaBody.setAttribute("class", "media-body")
+						let mediaHeading = document.createElement("h5")
+						mediaHeading.innerHTML = ` <bold>Description</bold>: ${invoice.description}`
+						invoiceList.appendChild(image)
+						mediaBody.appendChild(mediaHeading)
+						invoiceList.appendChild(mediaBody)
+						pendingListContainer.appendChild(invoiceList)
+				
+					}
+				}
+			})
+}
+
+
+
+function fetchEmployees(){
+	console.log("Button press")
+	fetch("http://localhost:8080/expensify/api/employees", {
+		headers: {
+			Authorization: `${token}`
+		}
+	}).then(res => {
+		if (res.status  ===  200){
+			return res.json()
+		} else {
+			console.log("error")
+		}
+	}).then(data => {
+		let pendingListContainer = document.getElementById("pendingListContainer")
+		pendingListContainer.hidden = true
+		let employeeListContainer = document.getElementById("employeeListContainer")
+		for (let employee of data){
+				console.log(employee)
+				let empList = document.createElement("div")
+				empList.setAttribute("class", "media")
+				let image = document.createElement("img")
+				image.setAttribute("class", "mr-3")
+				image.src= `https://mdbootstrap.com/img/Photos/Avatars/img%20(${employee.userId}).jpg`
+				let mediaBodyEmployee  = document.createElement("div")
+				mediaBodyEmployee.setAttribute("class", "media-body")
+				let mediaHeadingEmployee = document.createElement("h5")
+				mediaHeadingEmployee.innerHTML = employee.fullname
+				empList.appendChild(image)
+				mediaBodyEmployee.appendChild(mediaHeadingEmployee)
+				empList.appendChild(mediaBodyEmployee)
+				employeeListContainer.appendChild(empList)
+		}
+		})
+}
+
+
 
 
 function fetchResolveInvoices(){
@@ -44,14 +174,19 @@ function fetchResolveInvoices(){
 		}
 		})
 		.then(invoices => {
-			let invoiceList = document.getElementById("invoiceList")
-			invoiceList.hidden = false
-			
+			let pendingListContainer = document.getElementById("pendingListContainer")
+			pendingListContainer.style.display = "none"
+			let employeeListContainer = document.getElementById("employeeListContainer")
+			employeeListContainer.style.display = "none"
+			let	listResolvedInvoices = document.getElementById("listResolvedInvoices")
+			listResolvedInvoices.hidden = false
+		
 			for (let invoice of invoices){
-				let div = document.createElement("div")
-			if (invoice.resolved === true && invoice.pending === false) {
-				div.innerHTML = `${invoice.description}`
-				invoiceList.appendChild(div)
+				if (invoice.resolved === true && invoice.pending === false) {
+				let li = document.createElement("li")
+				li.setAttribute("class", "list-group-item list-group-item-success")
+				li.innerHTML = `${invoice.description}`
+				listResolvedInvoices.appendChild(li)
 				console.log(invoice)
 			}
 			}
@@ -59,6 +194,8 @@ function fetchResolveInvoices(){
 	
 	
 }
+
+
 
 function fetchMyResolvedInvoices(){
 	fetch(invoiceURL, {
@@ -91,32 +228,6 @@ function fetchMyResolvedInvoices(){
 }
 
 
-function sendAjaxGetInvoices(){
-	fetch("http://localhost:8080/expensify/api/invoices", {
-		headers: {
-			Authorization: `${token}`
-		}
-	})
-	.then(res => {
-		if (res.status === 200) {
-			return res.json()
-		} else {
-			console.log("error")
-		}
-		})
-		.then(invoices => {
-			let invoiceList = document.getElementById("invoiceList")
-			invoiceList.hidden = false
-			for (let invoice of invoices){
-				let div = document.createElement("div")
-			if (invoice.pending === true) {
-				div.innerHTML = `${invoice.description}`
-				invoiceList.appendChild(div)
-				console.log(invoice)
-			}
-			}
-		})
-}
 	
 
 function fetchMyPendingInvoices(){
@@ -195,38 +306,6 @@ function postInvoice(url, callback){
 
 
 
-function fetchEmployees(){
-	console.log("Button press")
-	fetch("http://localhost:8080/expensify/api/employees", {
-		headers: {
-			Authorization: `${token}`
-		}
-	}).then(res => {
-		if (res.status  ===  200){
-			return res.json()
-		} else {
-			console.log("error")
-		}
-	}).then(data => {
-		let carousel = document.getElementById("myCarousel")
-		carousel.hidden="false"
-			
-		let carouselInner = document.getElementById("myCarousel")
-		for (let employee of data){
-			console.log(employee)
-			let image = document.createElement("img")
-			image.src = `https://mdbootstrap.com/img/Photos/Avatars/img%20(${employee.userId}).jpg`
-			image.alt = `${employee.fullname}`
-
-			let name = document.createElement("h4")
-			name.innerText = `${employee.fullname}`
-		
-			
-	
-			
-		}})}
-
-
 function populate(xhr){
 	let invoiceJson = JSON.parse(xhr.response)
 	console.log(invoiceJson)
@@ -288,40 +367,6 @@ function denyInvoice(){
 
 
 
-function sendAjaxGet(url, callback){
-	let xhr = new XMLHttpRequest();
-	xhr.open("GET", url);
-	xhr.onreadystatechange = function(){
-		if(this.readyState === 4 && this.status === 200) {
-			callback(this);
-		} 
-	}
-	xhr.setRequestHeader("Authorization", token);
-	xhr.send();
-}
-
-function displayInfo(xhr) {
-	let empl = JSON.parse(xhr.response);
-	console.log(empl)
-	document.getElementById("full_name").innerHTML = empl.fullname
-	let image = document.getElementById('pic')
-	image.src = `https://mdbootstrap.com/img/Photos/Avatars/img%20(${currentUser}).jpg`
-	if (empl.manager === true) {
-		document.getElementById("search").hidden = false
-		document.getElementById("manager_status").innerHTML = "Manager"
-		document.getElementById("fetchInvoice").hidden = false
-		document.getElementById("fetchEmployee").hidden = false
-		console.log("A manager logged in")
-	} else {
-		document.getElementById("fetchInvoice").hidden = true
-		document.getElementById("approveOrDeny").style.display = "none"
-		document.getElementById("fetchEmployee").hidden = true
-		document.getElementById("search").hidden = true
-		document.getElementById("fetchResolved").hidden = true
-		console.log("A regular employee logged in")
-	}
-}
-
 function invoiceSearchByEmployeeId() {
 
 	let userid = document.getElementById("id").value 
@@ -333,10 +378,14 @@ function invoiceSearchByEmployeeId() {
 	.then(res => res.json())
 	.then(invoiceJson => {
 		let div =  document.getElementById("locatedInvoice")
+		let image = document.createElement("img")
+		div.hidden = false
 		for (let invoice of invoiceJson) {
 			let h1 = document.createElement('h1')
 			h1.innerText = invoice.description
+			image.src = `https://mdbootstrap.com/img/Photos/Avatars/img%20(${invoice.userId}).jpg` 
 			div.appendChild(h1)
+			div.prepend(image)
 		}
 		
 	})
