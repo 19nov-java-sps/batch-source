57e1c7f2baf88885e9fe243c4d43b983e3f02117
carolynrehm <carolynhrehm@gmail.com>
2019-12-19 15:00:16 -0500

    completed microservices

--- Week7-Microservices/CustomerService/src/main/java/com/revature/intercom/AccountClient.java
+++ Week7-Microservices/CustomerService/src/main/java/com/revature/intercom/AccountClient.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.revature.models.Account;
 
-@FeignClient(name="account-service")
+@FeignClient(name="account-service", fallback=AccountClientFallback.class)
 public interface AccountClient {
 	
 	@GetMapping("accounts/customer/{customerId}")
--- Week7-Microservices/GatewayService/src/main/java/com/revature/Application.java
+++ Week7-Microservices/GatewayService/src/main/java/com/revature/Application.java
@@ -2,20 +2,24 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.revature.filters.LoggingFilter;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableFeignClients
-@EnableCircuitBreaker
-@EnableHystrixDashboard
+@EnableZuulProxy
 public class Application {
 	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
+	}
+	 
+	@Bean 
+	public LoggingFilter loggingFilter() {
+		return new LoggingFilter();
 	}
 
 
