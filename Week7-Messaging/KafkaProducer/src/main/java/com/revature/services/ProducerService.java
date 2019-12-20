660ea8b1ead185502ecdada4a322b8c286649e42
carolynrehm <carolynhrehm@gmail.com>
2019-12-20 11:37:55 -0500

    kafka with spring

---
+++ Week7-Messaging/KafkaProducer/src/main/java/com/revature/services/ProducerService.java
@@ -0,0 +1,23 @@
+package com.revature.services;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProducerService {
+	
+	private static Logger log = Logger.getLogger(ProducerService.class);
+	
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+	
+	public void publishMessageToKafkaTopic(String message) {
+		log.info("producing message -----> "+ message);
+		kafkaTemplate.send("kafka-demo", message);
+	}
+	
+	
+
+}
